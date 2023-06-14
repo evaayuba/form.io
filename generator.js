8b9a1ad0731d31418fbe42641e12203d74795e22
evaayuba <yubaeva@29gmail.com>
2023-06-13 19:47:16 -0600

    badly commit

---
+++ generator.js
@@ -0,0 +1,30 @@
+    // Global data array
+    let data = ["syllabus.html", "p1.html", "p2.html", "style.html", "email.html", "system.html", "process.html"];
+
+    // Function to display the table
+    function displayTable() {
+      var startRange = parseInt(document.getElementById("startRange").value);
+      var endRange = parseInt(document.getElementById("endRange").value);
+
+      if (isNaN(startRange) || isNaN(endRange) || startRange > endRange || startRange < 1 || endRange > data.length) {
+        var validRange = "Valid Range: 1-" + data.length;
+        alert("Invalid range, " + validRange);
+        return;
+      }
+
+      var table = "<table><tr><th>Links</th></tr>";
+
+      for (var i = startRange - 1; i <= endRange - 1; i++) {
+        table += "<tr><td><a href='" + data[i] + "'>" + data[i] + "</a></td></tr>";
+      }
+
+      table += "</table>";
+
+      document.getElementById("tableContainer").innerHTML = table;
+    }
+
+    // Function to display the initial valid range message
+    function displayValidRangeMessage() {
+      var validRangeMessage = "Valid Range: 1-" + data.length;
+      document.getElementById("validRange").textContent = validRangeMessage;
+    }
